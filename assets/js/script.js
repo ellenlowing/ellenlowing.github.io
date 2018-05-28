e4f6aa2ec4de47aa38aed599ce91a85f52505246
Ellen Lo <lowing@bu.edu>
2018-05-28 15:47:04 -0400

    mobile test

--- assets/js/script.js
+++ assets/js/script.js
@@ -428,11 +428,12 @@ function leapAnimate( frame ) {
 */
 
 function isMobile() {
-    if(navigator.userAgent.indexOf("Mobile") == -1) console.log("this is NOT a mobile device");
-    else {
-        console.log("this is a mobile device");
-        var text = document.getElementById("text-guide");
+    var md = new MobileDetect(window.navigator.userAgent);
+    if(md.mobile()){
+        var text = document.getElementById(text-guide);
         text.innerHTML = "this is a mobile device";
+    }else{
+        console.log("this is not a mobile device");
     }
 }
 
