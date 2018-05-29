2ff0214a68ec606a24fedfe53adc3eaa7ffc069b
Ellen Lo <lowing@bu.edu>
2018-05-29 03:11:28 -0400

    add styling for mobile

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,4 +1,5 @@
 var script = document.createElement("script");
+var stylesheet = document.getElementById("stylesheet");
 function isMobile() {
     var md = new MobileDetect(window.navigator.userAgent);
     if(md.mobile()){
@@ -7,6 +8,7 @@ function isMobile() {
         document.body.appendChild(script);
         var guide = document.getElementById("text-guide");
         guide.innerHTML = "[double tap]: zoom in, [single tap]: pause, [e]: for ellen";
+        stylesheet.setAttribute("href", "assets/css/mobile.css");
     }else{
         console.log("this is not a mobile device");
         script.setAttribute('src','assets/js/script.js');
